09b922ddc9c0e8f0c752a52691572bc2d57d2d59
Xenor <xenordoz@gmail.com>
2026-02-11 01:45:42 +0100

    Forgot to add exercise 07 lesson 05

---
+++ exercises/05_lists/exercise07.py
@@ -0,0 +1,24 @@
+students = [
+    ("Alice", 15),
+    ("Bob", 12),
+    ("Charlie", 17)
+]
+
+# Create a dictionary named `grades`
+# Each key is the studend's name
+# Each value is their grade
+
+grades = {
+
+
+    
+}
+
+
+
+# Display the grade of `"Charlie"` from the dictionary
+
+
+
+
+# Add a new student `"Diana"` with grade `14`
---
+++ solutions/05_lists/exercise07.py
@@ -0,0 +1,23 @@
+students = [
+    ("Alice", 15),
+    ("Bob", 12),
+    ("Charlie", 17)
+]
+
+# Create a dictionary named `grades`
+# Each key is the studend's name
+# Each value is their grade
+
+grades = {
+    "Alice": 15,
+    "Bob": 12,
+    "Charlie": 17
+}
+
+# Display the grade of `"Charlie"` from the dictionary
+
+print(grades["Charlie"])
+
+# Add a new student `"Diana"` with grade `14`
+
+grades["Diana"] = 14
